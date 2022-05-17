b9242d14a8345c59e5f9223314911c7349cb27c4
danriley11 <dan_riley7@outlook.com>
2022-05-17 11:26:03 +1000

    Update
    + Form download successful; name of file is set to "Full Name" value

--- dnd_character_sheet/src/functions/form.js
+++ dnd_character_sheet/src/functions/form.js
@@ -1,15 +1,29 @@
 export default function handleFormSubmit(event) {
-    event.preventDefault();
-  
-    const data = new FormData(document.getElementById('test'));
-  
-    const formJSON = Object.fromEntries(data.entries());
-  
-    // for multi-selects, we need special handling
-    formJSON.snacks = data.getAll('snacks');
-  
-    //convert from inputs and outputs to clean innerText
-    const results = document.querySelector('.results pre');
-    results.innerText = JSON.stringify(formJSON, null, 2);
-    console.log(results.innerText)
+  event.preventDefault();
+
+  const data = new FormData(document.getElementById('test'));
+
+  const formJSON = Object.fromEntries(data.entries());
+
+  // for multi-selects, we need special handling
+  formJSON.snacks = data.getAll('snacks');
+
+  //convert from inputs and outputs to clean innerText
+  const results = document.querySelector('.results pre');
+  results.innerText = JSON.stringify(formJSON, null, 2);
+
+  // console.log(results.innerText)
+  const formOutput = results.innerText
+  console.log(formOutput);
+  downloadFormContent(formOutput, formJSON.name) //this will hold name of download
+
+  function downloadFormContent(formOutput, filename) {
+    const dataStr = "data:text/JSON;charset=utf-8," + encodeURIComponent(formOutput)
+    const downloadAnchorNode = document.createElement('a')
+    downloadAnchorNode.setAttribute("href", dataStr)
+    downloadAnchorNode.setAttribute("download", filename + ".json")
+    document.body.appendChild(downloadAnchorNode) //firefox proofing
+    downloadAnchorNode.click()
+    downloadAnchorNode.remove()
   }
+}
