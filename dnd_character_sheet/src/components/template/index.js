8d7efcfe9f05bc5d079dd40ff5d55a58e803c161
danriley11 <dan_riley7@outlook.com>
2022-05-31 19:17:12 +1000

    Update
    + Added ESLint
    + Updated Add/Remove Melee Attack
    ! Need to make Remove unique to each attack

---
+++ dnd_character_sheet/src/components/template/index.js
@@ -0,0 +1,15 @@
+// import React, { useState } from 'react';
+
+// import '../../styles/xyz.css'
+
+// import x from '../../functions/attack'
+
+function Template() {
+  const abc = 123;
+
+  return (
+      <div></div>
+  );
+}
+
+export default Template;
--- dnd_character_sheet/src/functions/form.js
+++ dnd_character_sheet/src/functions/form.js
@@ -6,25 +6,26 @@ export default function handleFormSubmit(event) {
   const formJSON = Object.fromEntries(data.entries());
 
   // for multi-selects, we need special handling
-  formJSON.snacks = data.getAll('snacks');
+  // formJSON.snacks = data.getAll('snacks');
 
-  //convert from inputs and outputs to clean innerText
+  // convert from inputs and outputs to clean innerText
   const results = document.querySelector('.results pre');
   results.innerText = JSON.stringify(formJSON, null, 2);
 
   // console.log(results.innerText)
-  const formOutput = results.innerText
+  const formOutput = results.innerText;
   console.log(formOutput);
-  downloadFormContent(formOutput, formJSON.name) //this will hold name of download
 
-  function downloadFormContent(formOutput, filename) {
-    const dataStr = "data:text/JSON;charset=utf-8," + encodeURIComponent(formOutput)
-    const downloadAnchorNode = document.createElement('a')
-    if (filename === '') {filename = 'DnD Character'}
-    downloadAnchorNode.setAttribute("href", dataStr)
-    downloadAnchorNode.setAttribute("download", filename + ".json")
-    document.body.appendChild(downloadAnchorNode) //firefox proofing
-    downloadAnchorNode.click()
-    downloadAnchorNode.remove()
+  function downloadFormContent() {
+    const dataStr = `data:text/JSON;charset=utf-8,${encodeURIComponent(formOutput)}`;
+    const downloadAnchorNode = document.createElement('a');
+    let filename = formJSON.name;
+    downloadAnchorNode.setAttribute('href', dataStr);
+    downloadAnchorNode.setAttribute('download', (filename = undefined ? `${filename}.json` : 'DnD Character'));
+    document.body.appendChild(downloadAnchorNode); // firefox proofing
+    downloadAnchorNode.click();
+    downloadAnchorNode.remove();
   }
+
+  downloadFormContent(formOutput, formJSON.name); // this will hold name of download
 }
